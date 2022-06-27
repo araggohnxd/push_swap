df6d03f5d3b0f27c458205449f6440a056a43d5f
araggohnxd <matheusnagato300@gmail.com>
2022-06-27 10:04:32 -0300

    Put error exit in separate file

--- sources/error.c
+++ sources/error.c
@@ -1,31 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_stack.c                                       :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/26 01:53:44 by maolivei          #+#    #+#             */
-/*   Updated: 2022/06/27 09:54:44 by maolivei         ###   ########.fr       */
+/*   Created: 2022/06/27 09:54:57 by maolivei          #+#    #+#             */
+/*   Updated: 2022/06/27 09:55:05 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_init_stack(t_stack_pair *stacks, int argc, char **argv)
+void	ft_error_and_exit(t_stack_pair *stacks)
 {
-	size_t	index;
-	t_llong	nbr;
-
-	index = 0;
-	while (++index < (size_t)argc)
-	{
-		if (!ft_is_number_str(argv[index]))
-			ft_error_and_exit(stacks);
-		nbr = ft_atoll(argv[index]);
-		if (nbr > INT_MAX || nbr < INT_MIN
-			|| ft_is_in_stack(nbr, stacks->stack_a))
-			ft_error_and_exit(stacks);
-		ft_stack_add_end(&stacks->stack_a, ft_stack_new((int) nbr));
-	}
+	ft_putendl_fd("Error", STDERR);
+	ft_stack_clear(&stacks->stack_a);
+	ft_stack_clear(&stacks->stack_b);
+	ft_stack_clear(&stacks->operations);
+	exit(EXIT_FAILURE);
 }
