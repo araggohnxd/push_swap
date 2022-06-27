b08e24ffa38bf7286e4bacc7eb2f10eee01aa634
araggohnxd <matheusnagato300@gmail.com>
2022-06-27 12:10:23 -0300

    Final tweaks to sort 100 under 700 operations

--- sources/split_stack.c
+++ sources/split_stack.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 23:25:40 by maolivei          #+#    #+#             */
-/*   Updated: 2022/06/26 04:46:01 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/06/27 11:52:05 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,51 +27,6 @@ static void	ft_push_remaining(t_stack_pair *stacks)
 			ft_push(stacks, PB);
 		else
 			ft_rotate(stacks, RA);
-	}
-}
-
-static void	ft_last_retrieve(t_stack_pair *stacks)
-{
-	size_t	stack_size;
-	int		b_biggest;
-	int		pivot;
-
-	stack_size = ft_stack_size(stacks->stack_b);
-	b_biggest = ft_get_biggest(stacks->stack_b);
-	pivot = (b_biggest / 2) + (b_biggest / 6);
-	while (--stack_size + 1)
-	{
-		if (ft_get_biggest(stacks->stack_b) < pivot + 1)
-		{
-			while (ft_stack_last(stacks->stack_b)->value > (b_biggest / 2) - 1
-				&& ft_stack_size(stacks->stack_b) > 1)
-				ft_reverse_rotate(stacks, RRB);
-			break ;
-		}
-		if (stacks->stack_b->value > pivot)
-			ft_push(stacks, PA);
-		else
-			ft_rotate(stacks, RB);
-	}
-}
-
-static void	ft_first_retrieve(t_stack_pair *stacks)
-{
-	size_t	stack_size;
-	int		b_biggest;
-	int		pivot;
-
-	stack_size = ft_stack_size(stacks->stack_b);
-	b_biggest = ft_get_biggest(stacks->stack_b);
-	pivot = b_biggest / 2;
-	while (--stack_size + 1)
-	{
-		if (ft_get_smallest(stacks->stack_b) > pivot + 1)
-			break ;
-		if (stacks->stack_b->value > pivot + 1)
-			ft_push(stacks, PA);
-		else
-			ft_rotate(stacks, RB);
 	}
 }
 
@@ -101,8 +56,5 @@ static void	ft_first_split(t_stack_pair *stacks)
 void	ft_split_stack(t_stack_pair *stacks)
 {
 	ft_first_split(stacks);
-	ft_first_retrieve(stacks);
-	ft_push_remaining(stacks);
-	ft_last_retrieve(stacks);
 	ft_push_remaining(stacks);
 }
