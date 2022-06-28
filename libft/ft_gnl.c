930d14aba293c55551c4b0c818189a99b02a33e7
araggohnxd <matheusnagato300@gmail.com>
2022-06-28 13:56:25 -0300

    Update libft

--- libft/ft_gnl.c
+++ libft/ft_gnl.c
@@ -6,110 +6,63 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 11:52:03 by maolivei          #+#    #+#             */
-/*   Updated: 2022/06/27 10:40:47 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/06/28 13:53:54 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/**
-* @brief Handles the static buffer, erasing its contents until it reaches
-* a newline. The purpose of this function is to discard data that has
-* already been written to the output in previous calls to get_next_line().
-* @param buffer The buffer to be handled.
-* @return A string containing what is in the buffer after the newline,
-* or NULL if an error occurs.
-*/
-static char	*ft_handle_buffer(char *buffer)
-{
-	size_t		i;
-	char		*str;
-
-	i = 0;
-	while (buffer[i] != '\n')
-		i++;
-	ft_strlcpy(buffer, &buffer[i + 1], (BUFFER_SIZE - i));
-	if (!*buffer)
-		return (NULL);
-	str = (char *) malloc(sizeof(char) * (BUFFER_SIZE - i + 1));
-	if (!str)
-		return (NULL);
-	ft_newlinecpy(str, buffer, 0);
-	return (str);
-}
-
-/**
-* @brief Reallocates and expands the memory of a string.
-* @param str The string to reallocate.
-* @param length The current length of the string.
-* @return A pointer to the reallocated string,
-* or NULL if an error occurs.
-*/
-static char	*ft_realloc(char *str, size_t length)
-{
-	char	*tmp;
-
-	if (!str)
-		str = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	else
-	{
-		tmp = (char *) malloc(sizeof(char) * (length + BUFFER_SIZE + 1));
-		ft_strlcpy(tmp, str, length + 1);
-		free(str);
-		str = tmp;
-	}
-	if (!str)
-		return (NULL);
-	return (str);
-}
-
-/**
-* @brief Calls function read() and write it to a buffer.
-* The buffer is then copied to a string. This process is repeated
-* until it reaches a newline or EOF of the given file descriptor.
-* @param fd The file descriptor to read from.
-* @param buffer The buffer to write what was read.
-* @param str The string containing the full line.
-* @return A pointer to a string containing the full line,
-* or NULL if an error occurs.
-*/
-static char	*ft_read(int fd, char *buffer, char *str)
+static char	*ft_extract_line(char *buffer, char *cache)
 {
 	size_t	i;
-	int		read_ret;
+	char	*aux;
 
+	if (!buffer)
+		return (NULL);
 	i = 0;
-	if (str)
-		i = ft_strlen(str);
-	read_ret = BUFFER_SIZE;
-	while (read_ret == BUFFER_SIZE)
+	while (buffer[i] != '\n' && buffer[i])
+		++i;
+	aux = ft_substr(buffer, 0, (i + 1));
+	if (buffer[i] && buffer[i + 1])
+		ft_strlcpy(cache, &buffer[i + 1], (ft_strlen(&buffer[i + 1]) + 1));
+	else
+		ft_bzero(cache, ft_strlen(cache));
+	free(buffer);
+	return (aux);
+}
+
+static char	*ft_read_line(int fd, char *buffer)
+{
+	int		read_ret;
+	char	*aux;
+
+	read_ret = TRUE;
+	while (read_ret)
 	{
-		read_ret = read(fd, buffer, BUFFER_SIZE);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+		aux = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		read_ret = read(fd, aux, BUFFER_SIZE);
 		if (read_ret < 1)
+		{
+			free(aux);
 			break ;
-		buffer[read_ret] = '\0';
-		str = ft_realloc(str, i);
-		i = ft_newlinecpy(str, buffer, i);
-		if (read_ret < BUFFER_SIZE || str[i - 1] == '\n')
-			break ;
+		}
+		aux[read_ret] = '\0';
+		buffer = ft_strjoin_free(&buffer, &aux);
 	}
-	return (str);
+	if (!*buffer)
+		ft_memfree((void *) &buffer);
+	return (buffer);
 }
 
 char	*ft_gnl(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
-	char		*str;
+	static char	cache[BUFFER_SIZE + 1];
+	char		*buffer;
 
 	if (fd < 0 || fd >= MAX_FD_VALUE || BUFFER_SIZE < 1)
 		return (NULL);
-	str = NULL;
-	if (ft_strchr(buffer, '\n'))
-	{
-		str = ft_handle_buffer(buffer);
-		if (str)
-			if (ft_strchr(str, '\n'))
-				return (str);
-	}
-	return (ft_read(fd, buffer, str));
+	buffer = ft_read_line(fd, ft_strdup(cache));
+	return (ft_extract_line(buffer, cache));
 }
